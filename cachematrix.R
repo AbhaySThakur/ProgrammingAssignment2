dcae157c49a742bb500d14ad427bae5dbea20f6e
AbhaySThakur <abhay.s.thakur@gmail.com>
2015-02-21 22:51:51 +0530

    Programming Assignment 2 submission 2 formatting changes

--- cachematrix.R
+++ cachematrix.R
@@ -13,15 +13,18 @@
 # e.g. yy <- makeCacheMatrix(matrix(c(1,2,3,2),nrow=2,ncol=2))
 #---------------------------------------------------------------------------
 makeCacheMatrix <- function(x = matrix()) {
-		m <- NULL				#initialize
-		set_mat <- function(y){	#function to assign values
-				x <<-y			#save data matrix 
-				m <<- NULL		#reset m
-		}		
-		get_mat <- function() x	#return data matrix
+        m <- NULL        		
+        set_mat <- function(y){	        #function to assign values
+                x <<-y			#save data matrix 
+                m <<- NULL		#reset m
+        }		
+        
+        get_mat    <- function() x	#return data matrix
         setmat_inv <- function(mat_inv) m <<- mat_inv
-        getmat_inv <- function() m		#return inverse matrix
-        list(set_mat = set_mat, get_mat = get_mat,
+        getmat_inv <- function() m	#return inverse matrix
+        
+        list(  set_mat  = set_mat, 
+               get_mat  = get_mat,
              setmat_inv = setmat_inv,
              getmat_inv = getmat_inv)
 }
@@ -39,13 +42,13 @@ makeCacheMatrix <- function(x = matrix()) {
 #---------------------------------------------------------------------------
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-		mat_inv <- x$getmat_inv()		
-		if(!is.null(mat_inv)) {		
-				message("getting cached data")
-				return(mat_inv)
-		}
-		mat_data <- x$get_mat()		#get stored data from matrixCacheMatix object
-		mat_inv <- solve(mat_data)	#calculate inverse of matrix
-		x$setmat_inv(mat_inv)		#save calculations for future use
-		mat_inv						
+        mat_inv <- x$getmat_inv()		
+        if(!is.null(mat_inv)) {		
+                message("getting cached data")
+                return(mat_inv)
+        }
+        mat_data <- x$get_mat()		#get stored data from matrixCacheMatix object
+        mat_inv  <- solve(mat_data)	#calculate inverse of matrix
+        x$setmat_inv(mat_inv)		#save calculations for future use
+        mat_inv						
 }
